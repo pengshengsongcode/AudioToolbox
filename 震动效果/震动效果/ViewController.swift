8d8c613d7116a7aca0071c6c4bbb66f3af382b08
pengshengsong <shengsongpeng@huangbaoche.com>
2016-10-20 14:44:33 +0800

    简单系统震动

---
+++ 震动效果/震动效果/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  震动效果
+//
+//  Created by 彭盛凇 on 16/10/20.
+//  Copyright © 2016年 huangbaoche. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //系统震动
+//        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+        
+        //系统声音
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
+        
+    }
+
+}
+
